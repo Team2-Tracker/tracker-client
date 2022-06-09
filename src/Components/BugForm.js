315db8687c32a79c17e46c917a6b6afbd42af368
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-06-08 19:54:46 -0500

    updating bugForm

--- src/Components/BugForm.js
+++ src/Components/BugForm.js
@@ -1,200 +1,210 @@
-import * as React from 'react'
-import { useState } from 'react'
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Stack from '@mui/material/Stack'
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
-import Button from '@mui/material/Button'
-import apiUrl from './../apiUrl'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
-import { now } from 'moment'
+import * as React from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import Button from "@mui/material/Button";
+import apiUrl from "./../apiUrl";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { now } from "moment";
 
 export default function BugForm(props) {
-	const { open, handleToggle, name, type, selected } = props
-	const [nameForm, setNameForm] = useState(
-		type === 'edit' ? selected.bugName : ''
-	)
-	const [issues, setIssues] = useState('')
-	const [priority, setPriority] = useState('')
-	const [estimate, setEstimate] = useState('')
-	const [dateDue, setDateDue] = useState(new Date(now()))
-	const [dateCreated, setDateCreated] = useState('')
-	const [assigned, setAssigned] = useState('')
-	const desktop = false
+  const { open, handleToggle, name, type, selected } = props;
+  const [nameForm, setNameForm] = useState(
+    type === "edit" ? selected.bugName : ""
+  );
+  const [issues, setIssues] = useState(type === "edit" ? selected.issues : "");
+  const [priority, setPriority] = useState(
+    type === "edit" ? selected.priority : ""
+  );
+  const [estimate, setEstimate] = useState(
+    type === "edit" ? selected.timeEstimate : ""
+  );
+  const [dateDue, setDateDue] = useState(new Date(now()),
+    type === "edit" ? selected.dateDue : ""
+  );
 
-	const handleNameChange = (event) => {
-		setNameForm(event.target.value)
-	}
+  const [dateCreated, setDateCreated] = useState(
+    type === "edit" ? selected.dateCreated : ""
+  );
+  const [assigned, setAssigned] = useState(
+    type === "edit" ? selected.assigned : ""
+  );
+  const desktop = false;
 
-	const handleIssuesChange = (event) => {
-		setIssues(event.target.value)
-	}
+  const handleNameChange = (event) => {
+    setNameForm(event.target.value);
+  };
 
-	const handlePriorityChange = (event) => {
-		setPriority(event.target.value)
-	}
+  const handleIssuesChange = (event) => {
+    setIssues(event.target.value);
+  };
 
-	const handleEstimateChange = (event) => {
-		setEstimate(event.target.value)
-	}
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value);
+  };
 
-	const handleDateChange = (event) => {
-		setDateDue(event)
-	}
+  const handleEstimateChange = (event) => {
+    setEstimate(event.target.value);
+  };
 
-	const handleDateCreatedChange = (event) => {
-		setDateCreated(event)
-	}
+  const handleDateChange = (event) => {
+    setDateDue(event);
+  };
 
-	const handleAssignedChange = (event) => {
-		setAssigned(event.target.value)
-	}
+  const handleDateCreatedChange = (event) => {
+    setDateCreated(event);
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault()
-		fetch(`${apiUrl}/bugs/`, {
-			method: type === 'edit' ? 'PATCH' : 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				bugName: nameForm,
-				issues: issues,
-				priority: priority,
-				timeEstimate: estimate,
-				dateDue: dateDue,
-				dateCreated: type === 'edit' ? dateCreated : new Date(now()),
-				assigned: assigned
-			})
-		}).then(() => {
-			setNameForm('')
-			setIssues('')
-			setPriority('')
-			setEstimate('')
-			setDateDue(null)
-			setAssigned('')
-			handleToggle()
+  const handleAssignedChange = (event) => {
+    setAssigned(event.target.value);
+  };
 
-			//   fetchBugs();
-		})
-	}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${apiUrl}/bugs/`, {
+      method: type === "edit" ? "PATCH" : "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        bugName: nameForm,
+        issues: issues,
+        priority: priority,
+        timeEstimate: estimate,
+        dateDue: dateDue,
+        dateCreated: type === "edit" ? dateCreated : new Date(now()),
+        assigned: assigned,
+      }),
+    }).then(() => {
+      setNameForm("");
+      setIssues("");
+      setPriority("");
+      setEstimate("");
+      setDateDue(null);
+      setAssigned("");
+      handleToggle();
 
-	let editDateCreated = ''
-	if (type === 'edit') {
-		editDateCreated = (
-			<MobileDatePicker
-				label="Date Created"
-				inputFormat="MM/DD/yyyy"
-				value={dateCreated}
-				onChange={handleDateCreatedChange}
-				renderInput={(params) => <TextField {...params} />}
-			/>
-		)
-	}
-	if (type == 'edit' && desktop) {
-		editDateCreated = (
-			<DesktopDatePicker
-				label="Date Created"
-				inputFormat="MM/DD/yyyy"
-				value={dateCreated}
-				onChange={handleDateCreatedChange}
-				renderInput={(params) => <TextField {...params} />}
-			/>
-		)
-	}
+      //   fetchBugs();
+    });
+  };
 
-	return (
-		<Dialog open={open} onClose={handleToggle}>
-			<DialogTitle>{name} Details</DialogTitle>
-			<DialogContent>
-				<Box
-					component="form"
-					sx={{
-						'& > :not(style)': { m: 1, width: '25ch' }
-					}}
-					noValidate
-					autoComplete="off"
-				>
-					<TextField
-						required
-						id="outlined-basic"
-						label="Name"
-						value={nameForm}
-						variant="outlined"
-						onChange={handleNameChange}
-					/>
-					<TextField
-						required
-						id="outlined-basic"
-						value={issues}
-						label="Issues"
-						variant="outlined"
-						onChange={handleIssuesChange}
-					/>
-					<FormControl fullWidth>
-						<InputLabel required id="demo-simple-select-label">
-							Priority
-						</InputLabel>
-						<Select
-							labelId="demo-simple-select-label"
-							id="demo-simple-select"
-							value={priority}
-							label="Priority"
-							onChange={handlePriorityChange}
-						>
-							<MenuItem value={1}>High</MenuItem>
-							<MenuItem value={2}>Medium</MenuItem>
-							<MenuItem value={3}>Low</MenuItem>
-						</Select>
-					</FormControl>
-					<TextField
-						required
-						id="outlined-basic"
-						value={estimate}
-						label="Estimated Hours"
-						variant="outlined"
-						onChange={handleEstimateChange}
-					/>
-					{desktop ? (
-						<DesktopDatePicker
-							label="Date Due"
-							inputFormat="MM/DD/yyyy"
-							value={dateDue}
-							onChange={handleDateChange}
-							renderInput={(params) => <TextField {...params} />}
-						/>
-					) : (
-						<MobileDatePicker
-							label="Date Due"
-							inputFormat="MM/DD/yyyy"
-							value={dateDue}
-							onChange={handleDateChange}
-							renderInput={(params) => <TextField {...params} />}
-						/>
-					)}
-					{editDateCreated}
-					<TextField
-						id="outlined-basic"
-						value={assigned}
-						label="Assigned"
-						variant="outlined"
-						onChange={handleAssignedChange}
-					/>
-				</Box>
-			</DialogContent>
-			<DialogActions>
-				<Button variant="contained" onClick={handleSubmit}>
-					Submit
-				</Button>
-			</DialogActions>
-		</Dialog>
-	)
+  let editDateCreated = "";
+  if (type === "edit") {
+    editDateCreated = (
+      <MobileDatePicker
+        label="Date Created"
+        inputFormat="MM/DD/yyyy"
+        value={dateCreated}
+        onChange={handleDateCreatedChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    );
+  }
+  if (type == "edit" && desktop) {
+    editDateCreated = (
+      <DesktopDatePicker
+        label="Date Created"
+        inputFormat="MM/DD/yyyy"
+        value={dateCreated}
+        onChange={handleDateCreatedChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    );
+  }
+
+  return (
+    <Dialog open={open} onClose={handleToggle}>
+      <DialogTitle>{name} Details</DialogTitle>
+      <DialogContent>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            required
+            id="outlined-basic"
+            label="Name"
+            value={nameForm}
+            variant="outlined"
+            onChange={handleNameChange}
+          />
+          <TextField
+            required
+            id="outlined-basic"
+            value={issues}
+            label="Issues"
+            variant="outlined"
+            onChange={handleIssuesChange}
+          />
+          <FormControl fullWidth>
+            <InputLabel required id="demo-simple-select-label">
+              Priority
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={priority}
+              label="Priority"
+              onChange={handlePriorityChange}
+            >
+              <MenuItem value={1}>High</MenuItem>
+              <MenuItem value={2}>Medium</MenuItem>
+              <MenuItem value={3}>Low</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            required
+            id="outlined-basic"
+            value={estimate}
+            label="Estimated Hours"
+            variant="outlined"
+            onChange={handleEstimateChange}
+          />
+          {desktop ? (
+            <DesktopDatePicker
+              label="Date Due"
+              inputFormat="MM/DD/yyyy"
+              value={dateDue}
+              onChange={handleDateChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          ) : (
+            <MobileDatePicker
+              label="Date Due"
+              inputFormat="MM/DD/yyyy"
+              value={dateDue}
+              onChange={handleDateChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          )}
+          {editDateCreated}
+          <TextField
+            id="outlined-basic"
+            value={assigned}
+            label="Assigned"
+            variant="outlined"
+            onChange={handleAssignedChange}
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 //  export default function BugForm() {
